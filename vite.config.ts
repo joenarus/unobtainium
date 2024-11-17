0ac8a434dd52a82704e3cd2bf4538716479662f8
joenarus <joe.narus2@gmail.com>
2024-11-17 14:57:34 -0700

    ts config updates

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   plugins: [
     react(),
     libInjectCss(),
-    dts({ include: ['lib'] })
+    dts({ include: ['lib'], rollupTypes: true, tsconfigPath: 'tsconfig.lib.json' })
   ],
   build: {
     copyPublicDir: false,
